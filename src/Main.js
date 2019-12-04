03cf1c16eeccecd5b323780142ed9c8e4aff437d
Evoxus <michael@michaelhploughman.com>
2019-12-03 21:48:43 -0700

    Passing student name as prop

--- src/Main.js
+++ src/Main.js
@@ -6,7 +6,7 @@ import Experience from './components/Experience';
 function Main() {
   return (
     <div>
-      <Address />
+      <Address studentName="Sally Student"/>
       <Education />
       <Experience/>
     </div>
--- src/components/Address.js
+++ src/components/Address.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-function Address() {
+function Address(props) {
   return (
     <address>
       <div className="contactInfo">
         <h3>Contact me</h3>
-        <p>Sally Student</p>
-        <p><a href="mailto:sallystudent@sallystudent.com" target="_blank">sallystudent@sallystudent.com</a></p>
+        <p>{props.studentName}</p>
+        <p><a href="mailto:{props.studentName.split(' ').join('')}@{props.studentName.split(' ').join('')}.com" target="_blank">{props.studentName.split(' ').join('')}@{props.studentName.split(' ').join('')}.com</a></p>
         <p>(123)555-7891</p>
       </div>
       <div className="imgContainer">
       <img src="https://m.media-amazon.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_.jpg"
-        alt="Profile pic of me, Sally Student"></img>
+        alt="Profile pic of me, {props.studentName.split(' ').join('')}"></img>
       </div>
     </address>
   );
